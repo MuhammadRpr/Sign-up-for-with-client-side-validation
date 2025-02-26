162d15f51a481dda3cf8054a9763d5ecbd357087
MuhammadRpr <muhammadqu017@gmail.com>
2025-02-26 14:10:39 +0700

    selesai C-5

---
+++ script.js
@@ -0,0 +1,31 @@
+let submitBtn = document.getElementById("submitBtn");
+
+// bikin suatu fungsi yang melakukan pengecekan terhadap input-input yang dimasukkan user
+function HandleClick(event) {
+    event.preventDefault()
+    //1. ambil value dari masing masing input
+    let firstName = document.getElementById("firstName").value.trim();
+    let lastName = document.getElementById("lastName").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    //2. Setelah kita mengambil value, kita mau beritahu user, klo input kosong dikasih peringatan
+    // 2. A. Ketika input firstname kosong, tolong masukkan teks "First name is required"
+    if (!firstName) {
+        document.getElementById("errorFirstName").innerText = "First name is required";
+        document.getElementById("firstName").classList.add("input-alert")
+    }
+
+    if (firstName && lastName && email && password) {
+        // ganti semua element di dalam div.form-section dengan element HTML baru (pada kasus ini adalah ucapan terima tengkyuuu)
+
+        document.querySelector(".form-section").innerHTML = `
+            <h2>Tyy bet! ${firstName} ${lastName}</h2>
+            <p>Contoh orang yang selamat dari kiamat tahun 2012 ${email}</p>
+        
+        `
+    }
+
+}
+
+submitBtn.addEventListener("click", HandleClick)
